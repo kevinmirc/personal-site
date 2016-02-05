d07a0927267f9c70239010414c14741c5142e1df
Kevin Mircovich <kmircovich1@gmail.com>
2016-02-04 20:44:30 -0500

    imported theme

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,37 @@
+// This is a manifest file that'll be compiled into application.js, which will include all the files
+// listed below.
+//
+// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
+// or any plugin's vendor/assets/javascripts directory can be referenced here using a relative path.
+//
+// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
+// compiled file.
+//
+// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
+// about supported directives.
+//
+//= require jquery
+//= require bootstrap-sprockets
+//= require jquery_ujs
+//= require turbolinks
+//= require_tree .
+
+//= require jquery-ui.min
+//= require plugin/jquery.easing
+
+//= require plugin/jquery.flexslider
+//= require plugin/background-check.min
+//= require plugin/jquery.fitvids
+//= require plugin/jquery.viewportchecker
+//= require plugin/jquery.stellar.min
+//= require plugin/wow.min
+//= require plugin/jquery.colorbox-min
+//= require plugin/owl.carousel.min
+//= require plugin/isotope.pkgd.min
+//= require plugin/masonry.pkgd.min
+//= require plugin/imagesloaded.pkgd.min
+//= require plugin/jPushMenu
+//= require plugin/jquery.fs.tipper.min
+//= require plugin/mediaelement-and-player.min
+//= require theme
+//= require navigation
