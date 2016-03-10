d7ee091647c87c91fb13ed699638ba26653b2e30
Kevin Mircovich <kmircovich1@gmail.com>
2016-03-09 16:25:30 -0800

    removed pry

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,5 @@
 class StaticPagesController < ApplicationController
   def index
-    binding.pry
     Keen.publish(:page_visit, {page: "home", visitor: request.remote_ip, from: request.from}) 
   end
 end
